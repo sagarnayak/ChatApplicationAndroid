4ee9b2d074c9d72bf9b855d351d3cafd717753d8
sagar kumar nayak <snkumar.nayak@gmail.com>
2019-06-13 14:31:05 +0530

    commit

--- app/src/main/java/com/sagar/android/chatapp/MainActivity.java
+++ app/src/main/java/com/sagar/android/chatapp/MainActivity.java
@@ -1,17 +1,21 @@
 package com.sagar.android.chatapp;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
 
-import org.json.JSONException;
-
+import java.lang.reflect.Array;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "sfbdbfgn";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +24,55 @@ public class MainActivity extends AppCompatActivity {
 
         try {
             IO.Options options = new IO.Options();
-            options.query = "token=dvdvdfvvs";
-            Socket socket = IO.socket("http://192.168.0.101:3000/", options);
+            options.query = "token=the dummy jwt token here";
+            final Socket socket = IO.socket("http://192.168.1.145:3000", options);
+
+            socket.on(
+                    "connect",
+                    new Emitter.Listener() {
+                        @Override
+                        public void call(Object... args) {
+                            Log.i(TAG, "socket connected");
+
+                            socket.emit("msg_from_client","Hello server");
+                        }
+                    }
+            );
+
+            socket.on(
+                    "msg_from_server",
+                    new Emitter.Listener() {
+                        @Override
+                        public void call(Object... args) {
+                            Log.i(TAG,"Server : "+ Arrays.toString(args));
+                        }
+                    }
+            );
+
+            socket.on(
+                    "disconnect",
+                    new Emitter.Listener() {
+                        @Override
+                        public void call(Object... args) {
+                            Log.i(TAG, "socket disconnected");
+                        }
+                    }
+            );
+
+            socket.on(
+                    "error",
+                    new Emitter.Listener() {
+                        @Override
+                        public void call(Object... args) {
+                            Log.i(TAG, "socket auth" + Arrays.toString(args));
+                        }
+                    }
+            );
 
             socket.connect();
         } catch (URISyntaxException e) {
             e.printStackTrace();
+            Log.i(TAG, "socket failed : " + e.toString());
         }
     }
 }
